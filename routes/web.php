1fe8150c8b5f3ce2662d050c6215bfea43a4b9bb
pannca <pancaprasetia@smkwikrama.sch.id>
2025-12-25 08:40:44 +0700

    Fix auth redirect configuration

--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,18 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/auth', [AuthController::class, 'showAuthForm'])->name('auth');
 Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::get('/auth', [AuthController::class, 'showAuthForm'])->name('auth');
 });
 
 Route::middleware('auth')->group(function () {
-
     Route::get('/surah', [SurahController::class, 'index'])->name('surah.index');
     Route::get('/surah/{nomor}', [SurahController::class, 'show'])->name('surah.show');
-
     Route::get('/riwayat', [SurahController::class, 'riwayat'])->name('surah.riwayat');
     Route::get('/bookmarks', [SurahController::class, 'bookmarks'])->name('surah.bookmarks');
-
     Route::post('/surah/history', [SurahController::class, 'saveHistory'])->name('surah.history');
-
     Route::view('/about', 'surah.about')->name('about');
-
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
